974332764c59d7d1bddab44e939e1a56b165ec25
TwentyCCent <para.tu.llegar@gmail.com>
2021-11-20 23:40:20 +0100

    toppaz

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,13 +4,13 @@
       <v-form>
         <v-card class="elevation-8">
           <v-toolbar dark color="primary">
-            <v-toolbar-title>Sign In</v-toolbar-title>
+            <v-toolbar-title>Accéder à mon espace</v-toolbar-title>
           </v-toolbar>
           <v-card-text>
             <v-text-field
               id="email"
               required
-              prepend-icon="mdi-account"
+              prepend-icon="mdi-email"
               name="email"
               label="Email"
               type="text"
@@ -23,18 +23,18 @@
               @click:append="show1 = !show1"
               :type="show1 ? 'text' : 'password'"
               name="password"
-              label="Password"
+              label="Mot de passe"
             ></v-text-field>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn type="submit" color="primary" elevation="3"> Submit </v-btn>
+            <v-btn type="submit" color="primary" elevation="3"> Valider </v-btn>
             <!-- to="/project" -->
           </v-card-actions>
           <p class="forgotPassword text-right mt-2 mb-4">
-            <router-link to="/register">Create an account</router-link>
-            or
-            <router-link to="/forgotPassword">Forgot password ?</router-link>
+            <router-link to="/register">Créer un compte</router-link>
+            ou
+            <router-link to="/forgotPassword">Mot de passe oublié ?</router-link>
           </p>
         </v-card>
       </v-form>
