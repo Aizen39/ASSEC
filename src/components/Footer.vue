d7e3e1489ecf948238185d72c07d7f0b6231fbd3
TwentyCCent <para.tu.llegar@gmail.com>
2021-11-20 01:55:18 +0100

    auth

---
+++ src/components/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-footer app color="primary" dark padless>
+    <v-row justify="center" no-gutters>
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+        href="/"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+        Created By <strong>SushiTeam</strong> ©{{ new Date().getFullYear() }} —
+        All rights reserved
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data(){
+    return{
+      links: ["Page d'accueil", "Tableau de bord", "Mes recommandations",  "Prévisions été"],
+      }
+  },
+};
+</script>
+
+<style>
+</style>
+
+<style>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md4>
+      <v-form>
+        <v-card class="elevation-8">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Sign In</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-text-field
+              id="email"
+              required
+              prepend-icon="mdi-account"
+              name="email"
+              label="Email"
+              type="text"
+            ></v-text-field>
+            <v-text-field
+              required
+              id="password"
+              prepend-icon="mdi-lock"
+              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="show1 = !show1"
+              :type="show1 ? 'text' : 'password'"
+              name="password"
+              label="Password"
+            ></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn type="submit" color="primary" elevation="3"> Submit </v-btn>
+            <!-- to="/project" -->
+          </v-card-actions>
+          <p class="forgotPassword text-right mt-2 mb-4">
+            <router-link to="/register">Create an account</router-link>
+            or
+            <router-link to="/forgotPassword">Forgot password ?</router-link>
+          </p>
+        </v-card>
+      </v-form>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+
+export default {
+  name: "Login",
+
+  data() {
+    return {
+        email: undefined,
+        password: undefined,
+        show1: false,
+  }
+  },
+  mounted() {
+
+  }
+  
+}
+</script>
+
+<style>
+.v-application p.forgotPassword {
+  font-size: 14px;
+  padding: 8px;
+  /* text-decoration: none;
+  text-decoration-line: none; */
+}
+
+</style>
