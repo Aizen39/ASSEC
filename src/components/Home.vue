6bd6ef651002748f76dba3c9bcd8668070e750d3
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2021-11-20 21:57:16 +0100

    maj home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,20 +2,30 @@
   <v-container>
     <v-row>
       <v-col cols="6" class="logo">
-        <v-img alt="assecc logo" class="logo-img" :src="require('../assets/Logos-Pictos/ASSECC-VF.png')"></v-img>
+        <v-img
+          alt="assecc logo"
+          class="logo-img"
+          :src="require('../assets/Logos-Pictos/ASSECC-VF.png')"
+        ></v-img>
       </v-col>
       <v-col cols="6" class="logo">
-        <v-img alt="crabfc logo" class="logo-img" :src="require('../assets/Logos-Pictos/Logo_CRABFC-S-VF.png')"></v-img>
+        <v-img
+          alt="crabfc logo"
+          class="logo-img"
+          :src="require('../assets/Logos-Pictos/Logo_CRABFC-S-VF.png')"
+        ></v-img>
       </v-col>
     </v-row>
 
     <v-row class="text-center">
       <v-col cols="12">
-        <h1 class="display-2 font-weight-bold mb-3">Bienvenue sur Agr'eau Durable</h1>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Bienvenue sur Agr'eau Durable
+        </h1>
       </v-col>
     </v-row>
 
-    <v-row style="text-align: center;">
+    <v-row style="text-align: center">
       <v-col cols="4">
         <v-img
           :src="require('../assets/btlLait.png')"
@@ -31,9 +41,6 @@
           contain
           height="200"
         />
-        <p class="subheading font-weight-regular">Solutions et ressources en élevage face au changement climatique.</p>
-        <p class="subheading font-weight-regular">Pour assurer performances, santé, bien-être, il est nécessaire de garantir un abreuvement suffisant</p>
-        <p class="subheading font-weight-regular">et de qualité. Agr'eau durable est la solution qui vous permet de maîtriser votre consommation d'eau.</p>
       </v-col>
       <v-col cols="4">
         <v-img
@@ -46,16 +53,20 @@
     </v-row>
 
     <v-row>
-      <p class="text-solution-home">Solutions et ressources en élevage face au changement climatique</p>
+      <p class="text-solution-home">Solutions et ressources en élevage face au changement climatique.</p>
+    </v-row>
+    <v-row>
+      <p class="text-solution-home">Pour assurer performances, santé, bien-être, il est nécessaire de garantir un abreuvement suffisant et de qualité.</p>
+    </v-row>
+    <v-row>
+      <p class="text-solution-home">Agr'eau durable est la solution qui vous permet de maîtriser votre consommation d'eau.</p>
     </v-row>
 
     <v-row>
       <v-col cols="12">
         <p></p>
       </v-col>
-      <v-col cols="6">
-        
-      </v-col>
+      <v-col cols="6"> </v-col>
     </v-row>
   </v-container>
 </template>
@@ -64,25 +75,23 @@
 export default {
   name: "HelloWorld",
 
-  data(){
-    return{
-
-    }
+  data() {
+    return {};
   },
 };
 </script>
 
 <style>
-.logo{
+.logo {
   margin: auto;
 }
-.logo .logo-img{
+.logo .logo-img {
   width: 40vh;
   margin: auto;
 }
-.text-solution-home{
+.text-solution-home {
   margin: auto;
   color: #f39200;
-    font-weight: bold;
+  font-weight: bold;
 }
 </style>
